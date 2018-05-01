0874c4b6de43b5fb54b19ea25d3003e0fda10b13
Emmanuel Anderson <manu1526@gmail.com>
2018-05-01 14:07:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+let name='Jillian';
+let holiday='Fruhlingsfest';
+let days=21;
+
+function happyHolidays(){
+  return "Happy Holidays!";
+}
+happyHolidays();
+
+function happyHolidaysTo(name){
+  return "`Happy holidays, ${name}!`";
+}
+happyHolidaysTo();
+
+function happyHolidayTo(name){
+  return "`Happy ${holiday}, ${name}!`";
+}
+happyHolidayTo();
+
+function holidayCountdown(days){
+  return "`It's ${days} days until ${holiday}!`"
+}
+holidayCountdown();
